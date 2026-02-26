756be75953b5033e23053715abb54e87ac5e9b87
nishan8299 <bhanupratapsingh7054003961@gmail.com>
2026-02-26 17:40:12 +0530

    update login logic with admin condition

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
 def login(username,possword):
    return f"User {username} logged in successfully with password {password}"
+
+def login(username,password):
+   if username == "admin":
+      return "welcome admin"
+   return f"User {username} logged in successfully"
